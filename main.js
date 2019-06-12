ef766d544ec046237b900d3c7d7d6827be12ef2f
Naruyoko <naru519372@gmail.com>
2019-06-12 08:58:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,15 +8,19 @@ var Entry=function(line){
     }
   }
   console.log(col);
-  var attributes="order,year,name,lname,author,lauthor,locale,expression,fgh,evolvedfrom,related".split(",");
+  //sets each listed properties
+  var attributes="order,year,name,lname,author,lauthor,locale,expression,fgh,evolvedfrom,related,color".split(",");
   for (var i=0;i<attributes.length;i++){
     this[attributes[i]]  = col[i]?col[i].split(": ")[1]:"";
   }
   this.evolvedfrom=this.evolvedfrom.split("/");
   this.related=this.related.split("/");
+  var img=new Image();
+  img.src="https://latex.codecogs.com/gif.latex?"+encodeURIComponent(this.expression);
+  this.image=img;
 }
 Entry.prototype.toString = function(){
-  return this.year + ":" + this.name + "(" + this.order + ") $$" + this.expression + "$$";
+  return this.year + ":" + this.name + "(" + this.order + ") $$" + this.expression + "$$ w/" + this.color;
 }
 Entry.prototype.pyear = function (){
   if (this.year>9999){
@@ -93,44 +97,70 @@ window.onload=function (){
 }
 
 function draw(){
+  //configure canvas
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
   canvas.width=960;
   canvas.height=640;
   ctx.fillStyle="white";
   ctx.fillRect(0,0,960,640);
+  //draw boxes
+  ctx.font="12px Courier";
+  ctx.lineWidth="1";
+  ctx.fillStyle="white"
   for (var i=0;i<data.length;i++){
+    //get data
     var datum=data[i];
     var pos=datum.drawPos();
+    //draw
+    ctx.beginPath();
+    ctx.strokeStyle=datum.color;
+    ctx.rect(pos.x-2,pos.y-12,ctx.measureText(datum.name).width+datum.image.naturalWidth*0.8+10,24);
+    ctx.stroke();
+  }
+  //draw entry
+  ctx.strokeStyle="black";
+  for (var i=0;i<data.length;i++){
+    //get data
+    var datum=data[i];
+    var pos=datum.drawPos();
+    //draw name
     ctx.fillStyle="black";
     ctx.font="12px Courier";
     ctx.fillText(datum.name,pos.x,pos.y);
-    var img=new Image();
-    img.src="https://latex.codecogs.com/gif.latex?"+encodeURIComponent(datum.expression);
+    //draw image with 80% scale
     ctx.scale(0.8,0.8);
-    ctx.drawImage(img,(pos.x+ctx.measureText(datum.name).width+5)*1.25,(pos.y-14)*1.25+4);
+    ctx.drawImage(datum.image,(pos.x+ctx.measureText(datum.name).width+5)*1.25,(pos.y-14)*1.25+4);
     ctx.scale(1.25,1.25);
+    //draw author and year
     ctx.fillStyle="gray";
     ctx.font="9px Courier";
     ctx.fillText(datum.author+", "+datum.year,pos.x,pos.y+10);
-    if (datum.evolvedfrom)
+    //evolved from/related handle
     for (var j=0;j<data.length;j++){
+      //get other
       var root=data[j-2];
       if (!root){
         continue;
       }
+      //evolved from
       ctx.font="12px Courier";
       if (datum.evolvedfrom.includes(root.name)||datum.evolvedfrom.includes(root.lname)){
         ctx.beginPath();
-        canvas_arrow(ctx,root.drawPos().x+ctx.measureText(root.name).width,root.drawPos().y,pos.x,pos.y);
+        ctx.strokeStyle=datum.color;
+        canvas_arrow(ctx,root.drawPos().x+ctx.measureText(root.name).width+root.image.naturalWidth*0.8+8,root.drawPos().y,pos.x,pos.y);
         ctx.stroke();
+        ctx.strokeStyle="black";
       }
+      //related
       if (datum.related.includes(root.name)||datum.related.includes(root.lname)){
         ctx.setLineDash([5,5]);
         ctx.beginPath();
-        canvas_arrow(ctx,root.drawPos().x+ctx.measureText(root.name).width,root.drawPos().y,pos.x,pos.y);
+        ctx.strokeStyle=datum.color;
+        canvas_arrow(ctx,root.drawPos().x+ctx.measureText(root.name).width+root.image.naturalWidth*0.8+8,root.drawPos().y,pos.x,pos.y);
         ctx.stroke();
         ctx.setLineDash([]);
+        ctx.strokeStyle="black";
       }
     }
   }
